a1e2ce9b6d26e8786893385793ee8199384d1cbe
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2021-06-04 19:13:00 +0000

    Added HTTPS support

---
+++ rest_api/config.js
@@ -0,0 +1,28 @@
+//Create and export config variables
+
+//container for all environments
+const environments = {}
+
+//staging (default)
+environments.staging = {
+    'httpPort': 3000,
+    'httpsPort': 3001,
+    'envName': 'staging'
+}
+
+//production (default)
+environments.production = {
+    'httpPort': 5000,
+    'httpsPort': 5001,
+    'envName': 'production'
+}
+
+//Determine which environment was passed as command line
+const currentEnvironment = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+//Determine if currentEnvironment is valid in environments object
+const appEnvironment = typeof(environments[currentEnvironment]) === 'object' ? environments[currentEnvironment] : environments.staging;
+
+module.exports = {
+    appEnvironment
+}
